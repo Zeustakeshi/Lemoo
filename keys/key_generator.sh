be91caa131ca9a4ddd6fdd5e606a292f0f8741c3
Minh Hiếu <gaconmvp2312@gmail.com>
2024-10-25 20:37:52 +0700

    create ci

--- keys/key_generator.sh
+++ keys/key_generator.sh
@@ -1,1 +1,14 @@
-openssl genrsa -out keypair.pem 2048 && openssl rsa -in keypair.pem -pubout -out publicKey.pem && openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keypair.pem -out privateKey.pem
+#!/bin/bash
+
+mkdir -p ./accessToken
+mkdir -p ./refreshToken
+
+# Create key pair for accessToken
+openssl genrsa -out ./accessToken/keypair.pem 2048
+openssl rsa -in ./accessToken/keypair.pem -pubout -out ./accessToken/publicKey.pem
+openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in ./accessToken/keypair.pem -out ./accessToken/privateKey.pem
+
+# Create key pair for refreshToken
+openssl genrsa -out ./refreshToken/keypair.pem 2048
+openssl rsa -in ./refreshToken/keypair.pem -pubout -out ./refreshToken/publicKey.pem
+openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in ./refreshToken/keypair.pem -out ./refreshToken/privateKey.pem

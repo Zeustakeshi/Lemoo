f61a80c80cd2f655ef40055d952f612d716965df
Minh Hiếu <gaconmvp2312@gmail.com>
2024-12-16 20:43:26 +0700

    init video service

---
+++ api/Video/src/test/java/com/lemoo/video/VideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemoo.video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

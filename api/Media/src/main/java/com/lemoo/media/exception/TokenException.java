77c23cc0fb485a0fca74cd8fa49cb7adcb63ddfa
Minh Hiếu <gaconmvp2312@gmail.com>
2024-12-26 00:30:24 +0700

    Initializes Media service
    Sets up the basic structure of the Media service with maven wrapper.
    Adds core dependencies for Spring boot, security, and OAuth2 resource server.
    Configures basic security and JWT decoder.
    Includes initial configurations for Cloudinary, Redis, and MongoDB.
    Also, adds base exception handling and security configurations.

---
+++ api/Media/src/main/java/com/lemoo/media/exception/TokenException.java
@@ -0,0 +1,13 @@
+/*
+ *  InvalidTokenException
+ *  @author: Minhhieuano
+ *  @created 10/19/2024 12:07 AM
+ * */
+
+package com.lemoo.media.exception;
+
+public class TokenException extends RuntimeException {
+    public TokenException(String message) {
+        super(message);
+    }
+}

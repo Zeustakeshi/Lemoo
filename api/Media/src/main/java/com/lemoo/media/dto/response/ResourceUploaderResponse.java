05521f5d412157d3cc37565e170a8cba1c1d9876
Minh Hiếu <gaconmvp2312@gmail.com>
2024-12-26 15:32:56 +0700

    Adds media service and gateway route
    Adds a new media service with basic image upload, retrieval, and deletion functionality.
    Sets up a gateway route for the media service, enabling external access.
    The Media service utilizes Cloudinary for image storage.
    Adds feign client to communicate with store-service for store verification.

---
+++ api/Media/src/main/java/com/lemoo/media/dto/response/ResourceUploaderResponse.java
@@ -0,0 +1,25 @@
+/*
+ *  ResourceUploaderResponse
+ *  @author: Minhhieuano
+ *  @created 12/10/2024 2:12 PM
+ * */
+
+package com.lemoo.media.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ResourceUploaderResponse {
+    private String publicId;
+    private Long version;
+    private String signature;
+    private Long width;
+    private Long height;
+    private String format;
+    private String resourceType;
+    private String url;
+    private String secureUrl;
+    private String etag;
+}

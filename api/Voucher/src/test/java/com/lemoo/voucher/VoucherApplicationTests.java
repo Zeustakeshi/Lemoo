2d473ac873b666397b204a8386d3af6c5fc69d1d
Minh Hiếu <gaconmvp2312@gmail.com>
2024-12-27 11:24:34 +0700

    Initializes Voucher API project
    Sets up the basic structure for the Voucher API, including project files, dependencies, configurations, and security settings.
    
    Adds maven wrapper, gitignore, pom.xml, and basic configurations.
    Initializes the base application with essential dependencies for security, data persistence, and web functionality.
    Sets up authentication using JWT.
    Configures exception handling and API response formats.

---
+++ api/Voucher/src/test/java/com/lemoo/voucher/VoucherApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lemoo.voucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherApplicationTests {
+
+	@Test
+	void contextLoads() {}
+}

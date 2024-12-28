53677ea6b36b186bcb2e0c30fde5b0d78d94e39f
Minh Hiếu <gaconmvp2312@gmail.com>
2024-12-28 16:13:32 +0700

    Refactors document upload process
    Improves document upload and verification by:
    - Replacing the synchronous document upload with asynchronous processing for better performance.
    - Removes unused event and consumer classes related to resource uploads.
    - Updates repository methods to directly update document URLs, avoiding unnecessary object fetching.
    - Moves document upload logic to a dedicated service and changes to use CompletableFuture for better performance and handling upload task.
    - Fixes API secret key for Cloudinary to be read from properties.
    - Adds `Enumerated` annotation to `StoreType` in the `Store` entity.

--- api/Store/src/main/java/com/lemoo/store/service/ResourceService.java
+++ api/Store/src/main/java/com/lemoo/store/service/ResourceService.java
@@ -7,16 +7,30 @@
 package com.lemoo.store.service;
 
 import com.lemoo.store.dto.response.ResourceUploaderResponse;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public interface ResourceService {
-	ResourceUploaderResponse uploadVideo(byte[] video, String publicId, String path) throws IOException;
+    ResourceUploaderResponse uploadImage(byte[] image, String publicId, String path) throws IOException;
 
-	ResourceUploaderResponse uploadImage(byte[] image, String publicId, String path) throws IOException;
+    CompletableFuture<ResourceUploaderResponse> uploadImageAsync(byte[] image, String publicId, String path);
 
-	void deleteVideo(String publicId, String path) throws IOException;
+    ResourceUploaderResponse uploadVideo(byte[] video, String publicId, String path) throws IOException;
 
-	void deleteImage(String publicId, String path) throws IOException;
+    CompletableFuture<ResourceUploaderResponse> uploadVideoAsync(byte[] video, String publicId, String path);
 
-	String generateSignedUrl(String publicId);
+    String deleteVideo(String publicId, String path) throws IOException;
+
+    CompletableFuture<String> deleteVideoAsync(String publicId, String path);
+
+    List<String> deleteImages(List<String> publicIds, String path) throws Exception;
+
+    CompletableFuture<List<String>> deleteImagesAsync(List<String> publicIds, String path);
+
+    String generateSignedUrl(String publicId);
+
+    byte[] ConvertMultipartToByte(MultipartFile file);
 }

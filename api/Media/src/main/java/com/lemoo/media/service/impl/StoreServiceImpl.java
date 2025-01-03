b9ca7efaeced5d06e9636bea987e281337dcedc2
Minh Hiếu <gaconmvp2312@gmail.com>
2025-01-04 00:01:02 +0700

    Adds Redis caching for store verification
    Integrates Redisson for caching store verification data
    Improves store verification performance by caching results
    Adds asynchronous caching for store verification requests

---
+++ api/Media/src/main/java/com/lemoo/media/service/impl/StoreServiceImpl.java
@@ -0,0 +1,53 @@
+/*
+ *  StoreServiceImpl
+ *  @author: Minhhieuano
+ *  @created 1/2/2025 12:24 AM
+ * */
+
+
+package com.lemoo.media.service.impl;
+
+import com.lemoo.media.client.StoreClient;
+import com.lemoo.media.dto.request.VerifyStoreRequest;
+import com.lemoo.media.exception.ForbiddenException;
+import com.lemoo.media.service.StoreService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBucket;
+import org.redisson.api.RedissonClient;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class StoreServiceImpl implements StoreService {
+
+    private final StoreClient storeClient;
+    private final RedissonClient redisson;
+
+    @Override
+    public void verifyStore(String accountId, String storeId) {
+        RBucket<String> bucket = redisson.getBucket(generateStoreOwnerVerifyKey(storeId));
+        String ownerId = bucket.get();
+        if (ownerId != null && ownerId.equals(accountId)) return;
+        boolean isStoreOwner = storeClient.verifyStore(new VerifyStoreRequest(accountId, storeId)).getData();
+        if (!isStoreOwner) throw new ForbiddenException("Only store owner can be modify product");
+        saveStoreOwnerToCacheAsync(accountId, storeId);
+    }
+
+    @Async
+    protected void saveStoreOwnerToCacheAsync(String accountId, String storeId) {
+        String key = generateStoreOwnerVerifyKey(storeId);
+        RBucket<String> bucket = redisson.getBucket(key);
+        try {
+            bucket.set(accountId, Duration.ofMinutes(30));
+        } catch (Exception ex) {
+            throw new RuntimeException("Save store verify to cache failed with key: " + key);
+        }
+    }
+
+    private String generateStoreOwnerVerifyKey(String storeId) {
+        return "store:" + storeId + ":owner";
+    }
+}

58700ded806fc496f4fdb5399ae85ba0f8c417ee
Minh Hiếu <gaconmvp2312@gmail.com>
2025-01-04 00:20:28 +0700

    Refactors product API endpoints and services
    Moves product endpoints and services to a new store namespace.
    Creates new recommendation endpoints and services for product features.
    Renames ProductController to ProductStoreController and ProductService to ProductStoreService.
    Adds initial implementation for ProductRecommendController and ProductRecommendService.

---
+++ api/Product/src/main/java/com/lemoo/product/controller/ProductRecommendController.java
@@ -0,0 +1,9 @@
+/*
+ *  ProductRecommendController
+ *  @author: Minhhieuano
+ *  @created 1/4/2025 12:06 AM
+ * */
+
+
+package com.lemoo.product.controller;public class ProductRecommendController {
+}

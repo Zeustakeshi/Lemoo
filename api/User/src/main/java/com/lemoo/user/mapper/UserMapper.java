3fc120b63ba7bbd5382f08d7407cc16121fb44b7
Minh Hiếu <gaconmvp2312@gmail.com>
2025-01-04 16:34:42 +0700

    Adds internal user API endpoint
    Creates endpoints for internal user information retrieval
    Implements necessary DTOs and mappers
    Includes service and service implementation layers for internal users

--- api/User/src/main/java/com/lemoo/user/mapper/UserMapper.java
+++ api/User/src/main/java/com/lemoo/user/mapper/UserMapper.java
@@ -6,11 +6,14 @@
 
 package com.lemoo.user.mapper;
 
+import com.lemoo.user.dto.response.InternalUserResponse;
 import com.lemoo.user.dto.response.UserResponse;
 import com.lemoo.user.entity.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-	UserResponse userToUserResponse(User user);
+    UserResponse userToUserResponse(User user);
+
+    InternalUserResponse toInternalUserResponse(User user);
 }

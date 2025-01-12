0cbd82d8c31f215365e3ecb259fdaf2f6ec53d26
Minh Hiếu <gaconmvp2312@gmail.com>
2025-01-12 11:26:15 +0700

    Adds Admin microservice
    Initializes Admin microservice project
    Includes necessary dependencies for Spring Boot, security, and JDBC
    Configures security, authentication, and authorization for Admin endpoints
    Implements base configurations for Kafka and JWT
    Creates basic API endpoints for admin

---
+++ api/Admin/src/main/java/com/lemoo/admin/config/JwtConfig.java
@@ -0,0 +1,30 @@
+/*
+ *  JwtConfig
+ *  @author: Minhhieuano
+ *  @created 10/29/2024 1:58 PM
+ * */
+
+
+package com.lemoo.admin.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class JwtConfig {
+
+    @Value("${services.auth-service}")
+    private String authServiceUrl;
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        String jwkSetUrl = authServiceUrl + "/internal/token/.well-known/jwks.json";
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUrl).build();
+    }
+}
---
+++ api/Admin/src/main/java/com/lemoo/admin/config/KafkaConfig.java
@@ -0,0 +1,21 @@
+/*
+ *  KafkaConfig
+ *  @author: Minhhieuano
+ *  @created 9/27/2024 9:10 PM
+ * */
+
+package com.lemoo.admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.converter.ByteArrayJsonMessageConverter;
+import org.springframework.kafka.support.converter.JsonMessageConverter;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public JsonMessageConverter jsonMessageConverter() {
+        return new ByteArrayJsonMessageConverter();
+    }
+}

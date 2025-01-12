8f45410b9caf31472ad9205eecab360ecbf765c6
Minh Hiếu <gaconmvp2312@gmail.com>
2025-01-12 11:26:15 +0700

    Adds Admin microservice
    Initializes Admin microservice project
    Includes necessary dependencies for Spring Boot, security, and JDBC
    Configures security, authentication, and authorization for Admin endpoints
    Implements base configurations for Kafka and JWT
    Creates basic API endpoints for admin

---
+++ api/Admin/src/main/java/com/lemoo/admin/dto/response/ApiResponse.java
@@ -0,0 +1,38 @@
+/*
+ *  ApiResponse
+ *  @author: Minhhieuano
+ *  @created 1/12/2025 11:16 AM
+ * */
+package com.lemoo.admin.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    private boolean isSuccess;
+    private T data;
+    private Object errors;
+    private LocalDateTime timestamp;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return ApiResponse.<T>builder()
+                .data(data)
+                .isSuccess(true)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    public static <T> ApiResponse<T> error(Object errors) {
+        return ApiResponse.<T>builder()
+                .isSuccess(false)
+                .errors(errors)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+}

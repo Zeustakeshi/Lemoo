3b085b14e5f7898f6b6b6323767b092b0f7d4aa5
Minh Hiếu <gaconmvp2312@gmail.com>
2025-01-20 20:14:17 +0700

    Adds UI components and dependencies for dialogs, forms, and selections
    Adds new components for buttons, dialogs, form controls (inputs, labels, descriptions, messages), and selects, including necessary styling and functionality.
    
    Integrates required dependencies (`@hookform/resolvers`, `@radix-ui/react-*`, `react-hook-form`, `zod`).
    
    Includes necessary updates to package.json and package-lock.json.
    
    Adds ThemeProvider for dynamic theme switching.

--- lemoo_mall/src/main.tsx
+++ lemoo_mall/src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "./context/ThemeContext";
 import "./index.css";
 import App from "./lib/App";
 
@@ -9,7 +10,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <App></App>
+            <ThemeProvider defaultTheme="light" storageKey="lemoo-mall-theme">
+                <App></App>
+            </ThemeProvider>
         </QueryClientProvider>
     </StrictMode>
 );
--- lemoo_mall/src/routes/__root.tsx
+++ lemoo_mall/src/routes/__root.tsx
@@ -1,14 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/context/ThemeContext";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import * as React from "react";
-
 export const Route = createRootRoute({
     component: RootComponent,
 });
 
 function RootComponent() {
+    const { setTheme } = useTheme();
     return (
         <React.Fragment>
-            <div className="text-2xl font-semibold">Hello "__root"!</div>
+            <div className="text-2xl font-semibold w-screen h-screen bg-blue-50 dark:bg-slate-900">
+                <Button onClick={() => setTheme("light")}>Light theme</Button>
+                <Button onClick={() => setTheme("dark")} variant="secondary">
+                    Dark theme
+                </Button>
+            </div>
             <Outlet />
         </React.Fragment>
     );

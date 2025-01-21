c89a23e41814fd29c74fe90810b2f7a4ded28a82
Minh Hiếu <gaconmvp2312@gmail.com>
2025-01-21 13:42:09 +0700

    Adds product feature section
    Adds a new product feature section to the home page
    Includes a carousel of featured products
    Creates ProductFeature and ProductFeatureCard components

---
+++ lemoo_mall/src/components/modules/product/ProductFeature.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import ProductFeatureCard from "./ProductFeatureCard";
+
+type Props = {};
+
+const ProductFeature = (props: Props) => {
+    return (
+        <div>
+            <div className="w-full flex justify-between items-center mb-3">
+                <h4 className="text-xl font-semibold ">Sản phẩm nổi bật</h4>
+                <Button variant="link">Xem tất cả</Button>
+            </div>
+            <div className="">
+                <Carousel
+                    opts={{
+                        loop: true,
+                    }}
+                    plugins={[
+                        Autoplay({
+                            delay: 4000,
+                        }),
+                    ]}
+                    className="w-full col-span-8"
+                >
+                    <CarouselContent className="">
+                        {Array.from({ length: 20 }).map((_, index) => (
+                            <CarouselItem
+                                className="w-max max-w-max"
+                                key={index}
+                            >
+                                <ProductFeatureCard></ProductFeatureCard>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                </Carousel>
+            </div>
+        </div>
+    );
+};
+
+export default ProductFeature;
--- lemoo_mall/src/routes/(home)/index.tsx
+++ lemoo_mall/src/routes/(home)/index.tsx
@@ -1,5 +1,6 @@
 import HomeBanner from "@/components/modules/banner/HomeBanner";
 import HomeCategoryList from "@/components/modules/categories/HomeCategoryList";
+import ProductFeature from "@/components/modules/product/ProductFeature";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/(home)/")({
@@ -10,6 +11,7 @@ function RouteComponent() {
     return (
         <div className="space-y-5">
             <HomeBanner></HomeBanner>
+            <ProductFeature></ProductFeature>
             <HomeCategoryList></HomeCategoryList>
         </div>
     );

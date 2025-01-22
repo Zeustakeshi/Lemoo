a4487a405a292d875fe4a6290a3f94dbe71dcf30
Minh Hiếu <gaconmvp2312@gmail.com>
2025-01-22 16:44:24 +0700

    Adds search functionality with history and suggestions
    Implements a global search bar with voice input and local search history
    Integrates Dexie database for persistent search history
    Adds components for displaying search suggestions and history.

---
+++ lemoo_mall/src/db/models/search.model.ts
@@ -0,0 +1,3 @@
+export interface SearchHistoryModel {
+    keyword: string;
+}
---
+++ lemoo_mall/src/hooks/useClickOutSide.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+
+export default function useClickOutSide(callback?: () => void) {
+    const nodeRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        function handleClickOutSide(this: Document, ev: MouseEvent) {
+            if (
+                nodeRef.current &&
+                !nodeRef.current.contains(ev.target as Node)
+            ) {
+                callback?.();
+            }
+        }
+        document.addEventListener("click", handleClickOutSide);
+        return () => {
+            document.removeEventListener("click", handleClickOutSide);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    return {
+        nodeRef,
+    };
+}

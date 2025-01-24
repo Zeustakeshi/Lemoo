322bb172d0e4e8718fdc9f1c5b9ca1a688c0ec13
Vohoangtuanhdhtdm <2224801030161@student.tdmu.edu.vn>
2025-01-24 22:09:32 +0700

    Zoom Image for detail Product

--- lemoo_mall/src/components/modules/product/product-detail/ProductImage.tsx
+++ lemoo_mall/src/components/modules/product/product-detail/ProductImage.tsx
@@ -1,46 +1,118 @@
 import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
+  Carousel,
+  CarouselContent,
+  CarouselItem,
 } from "@/components/ui/carousel";
+
+import { useRef, useState } from "react";
 
 type Props = {};
 
 const ProductImage = (props: Props) => {
-    return (
-        <div className="w-full">
-            <div className="aspect-square w-full border border-primary rounded-md bg-white dark:bg-slate-900">
-                <img
+  const [zoomStyle, setZoomStyle] = useState({
+    display: "none",
+    zoomX: "0%",
+    zoomY: "0%",
+  });
+
+  const imageZoomRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseMove = (event: React.MouseEvent) => {
+    if (imageZoomRef.current) {
+      const { offsetWidth, offsetHeight } = imageZoomRef.current;
+      const x = (event.nativeEvent.offsetX * 100) / offsetWidth;
+      const y = (event.nativeEvent.offsetY * 100) / offsetHeight;
+
+      setZoomStyle({
+        display: "block",
+        zoomX: `${x}%`,
+        zoomY: `${y}%`,
+      });
+    }
+  };
+
+  const handleMouseOut = () => {
+    setZoomStyle({
+      display: "none",
+      zoomX: "0%",
+      zoomY: "0%",
+    });
+  };
+  return (
+    <div className="w-full">
+      {/* Hình ảnh chính */}
+
+      <div
+        id="imageZoom"
+        ref={imageZoomRef}
+        style={
+          {
+            width: "550px",
+            height: "700px",
+            position: "relative",
+            "--url":
+              "url(https://i.pinimg.com/736x/d9/a6/61/d9a6614827a5b1ef4135119fb0e04398.jpg)",
+            "--zoom-x": zoomStyle.zoomX,
+            "--zoom-y": zoomStyle.zoomY,
+            "--display": zoomStyle.display,
+          } as React.CSSProperties
+        }
+        onMouseMove={handleMouseMove}
+        onMouseOut={handleMouseOut}
+      >
+        <img
+          src="https://i.pinimg.com/736x/d9/a6/61/d9a6614827a5b1ef4135119fb0e04398.jpg"
+          alt=""
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            objectPosition: "0 0",
+          }}
+        />
+        <div
+          style={{
+            display: zoomStyle.display,
+            content: '""',
+            width: "100%",
+            height: "100%",
+            backgroundColor: "black",
+            backgroundImage: `var(--url)`,
+            backgroundSize: "200%",
+            backgroundPosition: `${zoomStyle.zoomX} ${zoomStyle.zoomY}`,
+            position: "absolute",
+            left: 0,
+            top: 0,
+          }}
+        />
+      </div>
+
+      {/* Carousel */}
+      <Carousel className="w-full my-3">
+        <div className="relative">
+          <CarouselContent>
+            {Array.from({ length: 3 }).map((_, index) => (
+              <CarouselItem
+                key={index}
+                className="max-w-max w-max cursor-pointer"
+              >
+                <div className="w-[50px] p-1 rounded-md border border-primary aspect-square">
+                  <img
                     className="size-full object-contain"
-                    src="https://m.media-amazon.com/images/I/71DbpZqSGlL._AC_SX679_.jpg"
-                    alt=""
-                />
-            </div>
-            <Carousel className="w-full  my-3">
-                <div className="relative">
-                    <CarouselContent className="">
-                        {Array.from({ length: 3 }).map((image, index) => (
-                            <CarouselItem
-                                key={index}
-                                className="max-w-max w-max cursor-pointer"
-                            >
-                                <div className="  w-[50px] p-1 rounded-md border border-primary aspect-square">
-                                    <img
-                                        className="size-full object-contain"
-                                        src="https://picsum.photos/200"
-                                        alt=""
-                                    />
-                                </div>
-                                <p className="text-xs font-semibold text-muted-foreground">
-                                    đỏ - xl
-                                </p>
-                            </CarouselItem>
-                        ))}
-                    </CarouselContent>
+                    src="https://picsum.photos/200"
+                    alt={`Thumbnail ${index + 1}`}
+                  />
                 </div>
-            </Carousel>
+                <p className="text-xs font-semibold text-muted-foreground">
+                  đỏ - xl
+                </p>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
         </div>
-    );
+      </Carousel>
+    </div>
+  );
 };
 
 export default ProductImage;

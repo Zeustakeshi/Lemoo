dcfa7aabbc3a5dd05a20f4c2723a91646e9ee7be
Minh Hiếu <gaconmvp2312@gmail.com>
2025-01-27 16:24:00 +0700

    Refactors product display components
    Improves the structure and display of product recommendations in the cart.
    Introduces a new `ProductCard` component to streamline product presentation.
    Replaces `ProductFeature` with a more versatile `Carousel` component for showcasing similar products.

--- lemoo_mall/src/routes/cart/index.tsx
+++ lemoo_mall/src/routes/cart/index.tsx
@@ -1,5 +1,6 @@
 import { getCartInfo } from "@/api/cart.api";
 import CartItemGroup from "@/components/modules/cart/CartItemGroup";
+import ProductFeature from "@/components/modules/product/ProductFeature";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -19,18 +20,24 @@ function RouteComponent() {
         return <div className="text-2xl font-semibold">Đang tải</div>;
 
     return (
-        <div className="grid grid-cols-12">
-            <div className="col-span-8">
-                {data.items.map((cartItem) => (
-                    <CartItemGroup
-                        key={cartItem.id}
-                        cartItemId={cartItem.id}
-                        storeId={cartItem.storeId}
-                        skus={cartItem.skus}
-                    ></CartItemGroup>
-                ))}
+        <div>
+            <div className="grid grid-cols-12">
+                <div className="col-span-8">
+                    {data.items.map((cartItem) => (
+                        <CartItemGroup
+                            key={cartItem.id}
+                            cartItemId={cartItem.id}
+                            storeId={cartItem.storeId}
+                            skus={cartItem.skus}
+                        ></CartItemGroup>
+                    ))}
+                </div>
+                <div className="col-span-4 bg-orange-100"></div>
             </div>
-            <div className="col-span-4 bg-orange-100"></div>
+            <div className="my-5 space-y-4">
+                <ProductFeature></ProductFeature>
+                <ProductFeature></ProductFeature>
+            </div>
         </div>
     );
 }

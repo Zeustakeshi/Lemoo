d84c6942c66873d5ef7053f8fe5c2b1d57d7569f
Minh Hiếu <gaconmvp2312@gmail.com>
2025-02-02 17:50:15 +0700

    Migrates to external API and configures base URL
    Updates API calls to use a dedicated API library and external API endpoint.
    Configures the base URL for the API requests.
    Removes hardcoded mock API endpoint.

--- ui/admin/src/api/auth.api.ts
+++ ui/admin/src/api/auth.api.ts
@@ -1,13 +1,14 @@
 import { TokenType } from "@/common/type/token.type";
 import { User } from "@/common/type/user.type";
-import { token, user } from "@/data/auth.data";
-import { sleep } from "@/lib/utils";
+import { api } from "@/lib/api";
 import { LoginType } from "@/schemas/auth.schema";
 
 export const login = async (data: LoginType): Promise<TokenType> => {
-    return await sleep<TokenType>(token, 500);
+    // return await sleep<TokenType>(token, 500);
+    return await api.post("admin/auth/login", data);
 };
 
 export const getUseInfo = async (): Promise<User> => {
-    return await sleep<User>(user, 500);
+    // return await sleep<User>(user, 500);
+    return await api.get("/users/me");
 };

e12bf2e2ce0d0f27edb238ee2b0b16eba68ff272
Minh Hiếu <gaconmvp2312@gmail.com>
2025-02-06 10:22:31 +0700

    Adds core Chat entities and controller
    Creates `Room`, `Message`, `BaseEntity` entities, and `RoomController` for the chat API.  Includes basic CRUD structure for rooms.
    Implements necessary annotations and Lombok for entity classes.

---
+++ api/Chat/src/main/java/com/lemoo/chat/entity/Message.java
@@ -0,0 +1,27 @@
+/*
+ *  Message
+ *  @author: Minhhieuano
+ *  @created 2/6/2025 1:13 AM
+ * */
+
+
+package com.lemoo.chat.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@EqualsAndHashCode(callSuper = true)
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Data
+public class Message extends BaseEntity {
+    private String senderId;
+    private String roomId;
+    private String text;
+}

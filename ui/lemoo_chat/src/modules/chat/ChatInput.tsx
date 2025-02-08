3a6cbf8313b6cb7c83b4f59645018b9d99783606
Minh Hiếu <gaconmvp2312@gmail.com>
2025-02-08 18:02:51 +0700

    Adds message sending functionality to chat input
    Implements Enter key press for sending messages
    Handles message input and sending via a new function
    Includes a confirmation message in case of error

--- ui/lemoo_chat/src/modules/chat/ChatInput.tsx
+++ ui/lemoo_chat/src/modules/chat/ChatInput.tsx
@@ -1,10 +1,25 @@
 import { Separator } from "@/components/ui/separator";
 import TextArea from "@/components/ui/textarea";
+import { useState } from "react";
 import { IoSend } from "react-icons/io5";
 import { LuClipboardList, LuTicketPercent } from "react-icons/lu";
 type Props = {};
 
 const ChatInput = (props: Props) => {
+    const [message, setMessage] = useState<string>("");
+
+    const handleKeyDown = (e) => {
+        // Khi nhấn Enter mà không giữ Shift, gửi tin nhắn
+        if (e.key === "Enter" && !e.shiftKey) {
+            e.preventDefault(); // Ngăn không cho thêm ký tự xuống dòng
+            if (message.trim() !== "") {
+                // onSend(message); // Gọi hàm gửi tin nhắn từ props
+                alert("message: " + message);
+                setMessage(""); // Reset textarea sau khi gửi
+            }
+        }
+    };
+
     return (
         <div className="bg-white border-t">
             <div className="px-5 py-2 flex justify-start gap-2 items-center">
@@ -17,6 +32,9 @@ const ChatInput = (props: Props) => {
                     autoFocus
                     placeholder="Nhập tin nhắn của bạn"
                     className="focus-visible:ring-0 focus-visible:outline-none border-none outline-none  flex-1 col-span-11 px-0 h-full text-[16px] max-h-[100px]"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    onKeyDown={handleKeyDown}
                 />
                 <div className="col-span-1 flex justify-end items-center">
                     <button className="text-primary p-1 [&_svg]:size-[25px]">

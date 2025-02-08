14f5c97574795f052f24755208a651b10dfe4e30
Minh Hiếu <gaconmvp2312@gmail.com>
2025-02-08 11:04:57 +0700

    Adds pagination to message retrieval
    Implements pagination for retrieving messages from a room.
    Provides a new endpoint for fetching messages with support for paging and limiting results.
    Updates the repository and service layer to handle pagination requests.

--- api/Chat/src/main/java/com/lemoo/chat/repository/MessageRepository.java
+++ api/Chat/src/main/java/com/lemoo/chat/repository/MessageRepository.java
@@ -7,10 +7,12 @@
 package com.lemoo.chat.repository;
 
 import com.lemoo.chat.entity.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends MongoRepository<Message, String> {
-    
+    Page<Message> findAllByRoomId(String roomId, Pageable pageable);
 }

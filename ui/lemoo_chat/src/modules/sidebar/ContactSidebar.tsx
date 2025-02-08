b788d68a794a679d567d7f9be9c3790f441ac29a
Minh Hiếu <gaconmvp2312@gmail.com>
2025-02-08 14:17:50 +0700

    Adds contact sidebar items
    Improves the contact sidebar by adding distinct items for contacts, friend requests, and sent requests.
    Uses icons for better visual clarity.
    Includes links for navigation to the respective sections.

--- ui/lemoo_chat/src/modules/sidebar/ContactSidebar.tsx
+++ ui/lemoo_chat/src/modules/sidebar/ContactSidebar.tsx
@@ -1,5 +1,7 @@
 import Header from "@/components/header/Header";
 import { cn } from "@/lib/utils";
+import { Send, UserCheck, UsersRound } from "lucide-react";
+import ContactSidebarItem from "./ContactSidebarItem";
 
 type Props = {
     className?: string;
@@ -8,7 +10,18 @@ type Props = {
 const ContactSidebar = ({ className }: Props) => {
     return (
         <div className={cn("", className)}>
-            <Header></Header>{" "}
+            <Header></Header>
+            <div>
+                <ContactSidebarItem to="/contacts" icon={<UsersRound />}>
+                    Danh sách bạn bè
+                </ContactSidebarItem>
+                <ContactSidebarItem to="/contacts/request" icon={<UserCheck />}>
+                    Lời mời kết bạn
+                </ContactSidebarItem>
+                <ContactSidebarItem to="/contacts/sent" icon={<Send />}>
+                    Lời mời đã gửi
+                </ContactSidebarItem>
+            </div>
         </div>
     );
 };

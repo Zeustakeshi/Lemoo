aa7053998291e9923f0938e8f3d9eaf9899a2b6e
Minh Hiếu <gaconmvp2312@gmail.com>
2025-02-11 00:49:50 +0700

    Adds authentication to WebSocket communication
    Implements a channel interceptor to authenticate WebSocket connections.
    Integrates security by retrieving user information during WebSocket connection establishment.
    Adds a DTO for authenticated account information.

---
+++ api/Socket/src/main/java/com/lemoo/socket/config/AuthChannelInterceptor.java
@@ -0,0 +1,44 @@
+/*
+ *  AuthChannelInterceptor
+ *  @author: Minhhieuano
+ *  @created 2/11/2025 12:23 AM
+ * */
+
+
+package com.lemoo.socket.config;
+
+import com.lemoo.socket.dto.common.AuthenticatedAccount;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthChannelInterceptor implements ChannelInterceptor {
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor =
+                MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            String authHeader = accessor.getFirstNativeHeader("Authorization");
+            if (authHeader == null) return message;
+            String token = authHeader.substring(7);
+            AuthenticatedAccount account = callAuthServer(token);
+            accessor.setUser(new UsernamePasswordAuthenticationToken(account, null, null));
+        }
+        return message;
+    }
+
+    private AuthenticatedAccount callAuthServer(String token) {
+        System.out.println("token = " + token);
+        return AuthenticatedAccount.builder()
+                .email("example.user@gmail.com")
+                .userId("user-1")
+                .id("id-1")
+                .build();
+    }
+}

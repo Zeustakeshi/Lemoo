b1ebfbf90a413556d8359beda4ce324f9cab252d
Minh Hiếu <gaconmvp2312@gmail.com>
2025-02-11 09:37:08 +0700

    Adds infinite scrolling to contact lists
    Implements infinite scrolling for contact requests, recommendations, and all contacts.
    Uses `useInfiniteQuery` to fetch data in pages.
    Includes handling for last page and pagination.

--- ui/lemoo_chat/src/modules/contact/ContactList.tsx
+++ ui/lemoo_chat/src/modules/contact/ContactList.tsx
@@ -1,13 +1,36 @@
+import { getAllContact } from "@/api/friend.api";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import ContactItem from "./ContactItem";
 
 type Props = {};
 
 const ContactList = (props: Props) => {
+    const { data, fetchNextPage, hasNextPage, isFetchingNextPage, refetch } =
+        useInfiniteQuery({
+            queryKey: ["contact-list"],
+            queryFn: async ({ pageParam }) => await getAllContact(pageParam),
+            getNextPageParam: (lastPage: any) => {
+                if (lastPage.last) return undefined;
+                return lastPage.pageNumber + 1;
+            },
+            initialPageParam: 0,
+        });
+
+    console.log({ data });
+
     return (
         <div className="h-min bg-white p-4 rounded-xl space-y-2">
-            {Array.from({ length: 40 }).map((_, index) => (
-                <ContactItem key={index}></ContactItem>
-            ))}
+            {data &&
+                data?.pages
+                    .flatMap(({ content }: any) => content ?? [])
+                    .map((contact: any, index, content) => {
+                        const lastIndex = content.length - 1;
+                        if (index === Math.ceil(lastIndex * 0.8))
+                            return <ContactItem key={index} />;
+                        return <ContactItem key={index} />;
+                    })}
+
+            {!hasNextPage && status !== "pending" && <p>Hết rồi</p>}
         </div>
     );
 };

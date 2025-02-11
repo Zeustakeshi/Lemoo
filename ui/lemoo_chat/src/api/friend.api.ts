9d96bbb3f76bd0f0264de579f5baf4751d5fc447
Minh Hiếu <gaconmvp2312@gmail.com>
2025-02-11 09:15:08 +0700

    Adds friend request feature
    Implements friend request functionality
    Adds endpoints for sending, accepting, and rejecting friend requests
    Updates data types to handle friend requests
    Integrates toast notifications for feedback

--- ui/lemoo_chat/src/api/friend.api.ts
+++ ui/lemoo_chat/src/api/friend.api.ts
@@ -1,4 +1,4 @@
-import { UserContact } from "@/common/type/contact.type";
+import { ContactRequest, UserContact } from "@/common/type/contact.type";
 import { Pageable } from "@/common/type/page.type";
 import { api } from "@/lib/api";
 
@@ -7,12 +7,39 @@ export const getContactSuggestions = async (
     limit?: number
 ): Promise<Pageable<UserContact>> => {
     return await api.get("/users/friends/recommend", {
-        params: { page, limit: limit || 10 },
+        params: { page, limit: limit ?? 10 },
+    });
+};
+
+export const getContactRequests = async (
+    page: number,
+    limit?: number
+): Promise<Pageable<ContactRequest>> => {
+    return await api.get("/users/friends/request", {
+        params: {
+            page,
+            limit: limit ?? 10,
+        },
     });
 };
 
 export const sendContactRequest = async (target: string) => {
     return await api.post("/users/friends/request", {
         target,
+    });
+};
+
+export const acceptContactRequest = async (requestId: string) => {
+    console.log({ requestId });
+    return await api.post("/users/friends/accept", {
+        senderId: requestId,
+    });
+};
+
+export const rejectContactRequest = async (requestId: string) => {
+    return await api.delete("/users/friends/reject", {
+        data: {
+            senderId: requestId,
+        },
     });
 };
--- ui/lemoo_chat/src/modules/contact/FriendRequestCard.tsx
+++ ui/lemoo_chat/src/modules/contact/FriendRequestCard.tsx
@@ -1,32 +1,81 @@
+import { acceptContactRequest, rejectContactRequest } from "@/api/friend.api";
+import { ContactRequest } from "@/common/type/contact.type";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { useMutation } from "@tanstack/react-query";
 import moment from "moment";
+import toast from "react-hot-toast";
 
-type Props = {};
+type Props = {
+    contactRequest: ContactRequest;
+};
 
-const FriendRequestCard = (props: Props) => {
+const FriendRequestCard = ({ contactRequest }: Props) => {
+    const {
+        mutateAsync: acceptContactMutate,
+        isPending: acceptContactPending,
+    } = useMutation({
+        mutationKey: ["accept-friend", contactRequest.requestId],
+        mutationFn: async () =>
+            await acceptContactRequest(contactRequest.requestId),
+    });
+
+    const {
+        mutateAsync: rejectContactMutate,
+        isPending: rejectContactPending,
+    } = useMutation({
+        mutationKey: ["reject-friend", contactRequest.requestId],
+        mutationFn: async () =>
+            await rejectContactRequest(contactRequest.requestId),
+    });
+
+    const handleAcceptContactRequest = async () => {
+        try {
+            await acceptContactMutate();
+        } catch (error: any) {
+            toast.error("Kết bạn thất bại");
+        }
+    };
+
+    const handleRejectContactRequest = async () => {
+        try {
+            await rejectContactMutate();
+        } catch (error: any) {
+            toast.error("Từ chối kết bạn thất bại");
+        }
+    };
+
     return (
         <div className="bg-white rounded-xl shadow-sm p-4 max-w-[400px]  h-[160px]">
             <div className="flex justify-start items-start gap-2">
                 <Avatar className="size-[45px]">
-                    <AvatarImage src="https://i.pravatar.cc/150?img=31"></AvatarImage>
+                    <AvatarImage src={contactRequest.avatar}></AvatarImage>
                 </Avatar>
                 <div>
                     <h4 className="flex-1 line-clamp-2 font-medium">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Ea, dicta. Deserunt quos optio doloribus nostrum sunt
-                        voluptas fuga exercitationem maxime.
+                        {contactRequest.username}
                     </h4>
                     <span className="text-xs text-muted-foreground">
-                        {moment(new Date()).toNow()}
+                        {moment(contactRequest.timestamp).fromNow()}
                     </span>
                 </div>
             </div>
             <div className="flex justify-between items-center gap-4 mt-5">
-                <Button variant="secondary" className="w-full">
-                    Từ chối
+                <Button
+                    disabled={rejectContactPending}
+                    onClick={handleRejectContactRequest}
+                    variant="secondary"
+                    className="w-full"
+                >
+                    {rejectContactPending ? "Đang xử lý" : "Từ chối"}
                 </Button>
-                <Button className="w-full">Đồng ý</Button>
+                <Button
+                    disabled={acceptContactPending}
+                    className="w-full"
+                    onClick={handleAcceptContactRequest}
+                >
+                    {acceptContactPending ? "Đang xử lý" : "Đồng ý"}
+                </Button>
             </div>
         </div>
     );

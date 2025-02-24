c481647c8f200e0d42f0cd75b83f430ee4a780c5
Vohoangtuanhdhtdm <121412803+Vohoangtuanhdhtdm@users.noreply.github.com>
2025-02-24 18:46:48 +0700

    refactor-product-detail-3 (#209)
    Co-authored-by: Vohoangtuanhdhtdm <2224801030161@student.tdmu.edu.vn>

---
+++ ui/lemoo_mall/src/common/type/product.type.ts
@@ -0,0 +1,93 @@
+/**
+ * Dữ liệu trả về từ api
+ */
+export type TypeProdcutDetail = {
+  /**
+   * Mô tả sản phẩm
+   */
+  description: string;
+  /**
+   * Mã sản phẩm
+   */
+  id: string;
+  /**
+   * Danh sách ảnh sản phẩm
+   */
+  images: string[];
+  /**
+   * Cờ thể hiện sản phẩm còn hàng không
+   */
+  isSoldOut: boolean;
+  /**
+   * Tên sản phẩm
+   */
+  name: string;
+  /**
+   * Đánh giá sản phẩm
+   */
+  ratting: number;
+  /**
+   * Số lượt đánh giá sản phẩm
+   */
+  rattingCount: number;
+  skus: Skus[];
+  /**
+   * Mã cửa hàng
+   */
+  storeId: string;
+  /**
+   * Số lượng đã bán
+   */
+  totalSold: number;
+  /**
+   * Danh sách biến thể
+   */
+  variants: Variant[];
+  [property: string]: any;
+};
+
+export type Skus = {
+  /**
+   * Ảnh sku
+   */
+  image: string;
+  /**
+   * Mã sku
+   */
+  lemooSku: string;
+  /**
+   * Tên sku
+   */
+  name: string;
+  /**
+   * Giá gốc sku
+   */
+  originPrice: number;
+  /**
+   * Giá khuyến mãi sku
+   */
+  promotionPrice: number;
+  /**
+   * Biến thể tạo thành sku
+   */
+  variants: { [key: string]: any };
+  [property: string]: any;
+};
+
+export type Variant = {
+  /**
+   * Tên biến thể
+   */
+  name: string;
+  /**
+   * Giá trị biến thể
+   */
+  values: Value[];
+  [property: string]: any;
+};
+
+export type Value = {
+  code: string;
+  name: string;
+  [property: string]: any;
+};

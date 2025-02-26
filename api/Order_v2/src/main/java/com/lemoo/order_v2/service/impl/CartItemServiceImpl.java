595f5325dd65a44a405fdadab64a6dda0a02d6e0
Minh Hiếu <gaconmvp2312@gmail.com>
2025-02-26 23:27:59 +0700

    Adds SKU caching and service
    Introduces a new `SkuService` and `SkuServiceImpl` for fetching and caching SKU data.
    Implements caching mechanism using Redisson for SKU data.
    Creates `SkuCacheHelper` for generating cache keys.
    Removes the old `ProductService`.
    Replaces `ProductService` calls with `SkuService` calls.

--- api/Order_v2/src/main/java/com/lemoo/order_v2/service/impl/CartItemServiceImpl.java
+++ api/Order_v2/src/main/java/com/lemoo/order_v2/service/impl/CartItemServiceImpl.java
@@ -6,22 +6,17 @@
 
 package com.lemoo.order_v2.service.impl;
 
-import com.lemoo.order_v2.common.enums.CartItemStatus;
 import com.lemoo.order_v2.dto.common.AuthenticatedAccount;
-import com.lemoo.order_v2.dto.common.CartItemCache;
 import com.lemoo.order_v2.dto.request.AddToCartRequest;
 import com.lemoo.order_v2.dto.response.CartItemResponse;
 import com.lemoo.order_v2.dto.response.PageableResponse;
 import com.lemoo.order_v2.dto.response.SkuResponse;
 import com.lemoo.order_v2.entity.CartItem;
-import com.lemoo.order_v2.mapper.CartItemCacheMapper;
 import com.lemoo.order_v2.mapper.CartItemMapper;
 import com.lemoo.order_v2.mapper.PageMapper;
 import com.lemoo.order_v2.repository.CartItemRepository;
-import com.lemoo.order_v2.service.CartCacheService;
-import com.lemoo.order_v2.service.CartItemCacheService;
 import com.lemoo.order_v2.service.CartItemService;
-import com.lemoo.order_v2.service.ProductService;
+import com.lemoo.order_v2.service.SkuService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -34,10 +29,7 @@ import java.util.concurrent.CompletableFuture;
 @RequiredArgsConstructor
 public class CartItemServiceImpl implements CartItemService {
     private final CartItemRepository cartItemRepository;
-    private final ProductService productService;
-    private final CartItemCacheMapper cartItemCacheMapper;
-    private final CartItemCacheService cartItemCacheService;
-    private final CartCacheService cartCacheService;
+    private final SkuService skuService;
     private final CartItemMapper cartItemMapper;
     private final PageMapper pageMapper;
 
@@ -45,7 +37,7 @@ public class CartItemServiceImpl implements CartItemService {
     public String addToCart(AddToCartRequest request, AuthenticatedAccount account) {
 
         // Retrieve SKU information based on the provided SKU code from the request.
-        SkuResponse sku = productService.getSkuBySkuCode(request.getLemooSku());
+        SkuResponse sku = skuService.getSkuBySkuCode(request.getLemooSku());
 
         // Check if the cart item already exists for the user and SKU.
         Optional<CartItem> cartItemOptional =
@@ -71,34 +63,19 @@ public class CartItemServiceImpl implements CartItemService {
         // Save the updated or newly created cart item to the repository.
         cartItemRepository.save(cartItem);
 
-        //  Create and determine the status of the cart item cache based on stock availability.
-        CartItemCache cartItemCache = cartItemCacheMapper.toCartItemCache(cartItem);
-        cartItemCache.setStatus(
-                isOutOfStock(cartItem.getQuantity(), sku.getStock())
-                        ? CartItemStatus.OUT_OF_STOCK
-                        : CartItemStatus.ACTIVE);
-
-        // Save cart item with status to cache
-        cartItemCacheService.saveToCache(cartItemCache);
-        cartCacheService.addCartItemToCart(account.getUserId(), cartItem.getId());
         return cartItem.getId();
     }
 
     @Override
     public PageableResponse<CartItemResponse> getUserCart(int page, int limit, AuthenticatedAccount account) {
         PageRequest request = PageRequest.of(page, limit);
-        Page<String> cartItemIds = cartCacheService.getCartCache(account.getUserId(), request);
 
-        Page<CartItemResponse> responses = cartItemIds.map(cartItemId -> CompletableFuture.supplyAsync(() -> {
-            CartItemCache cartItemCache = cartItemCacheService.getCartItemCache(cartItemId);
-            return cartItemMapper.toCartItemResponse(cartItemCache);
+        Page<CartItem> cartItems = cartItemRepository.findAllByUserId(account.getUserId(), request);
 
-        })).map(CompletableFuture::join);
-
+        Page<CartItemResponse> responses = cartItems.map(cartItem -> CompletableFuture
+                        .supplyAsync(() -> cartItemMapper.toCartItemResponse(cartItem)))
+                .map(CompletableFuture::join);
         return pageMapper.toPageableResponse(responses);
     }
 
-    private boolean isOutOfStock(Integer cartQuantity, Long available) {
-        return available < cartQuantity;
-    }
 }

d0b66458d4638bfac405ecf44daa1878c8b3a9ff
Minh Hiếu <gaconmvp2312@gmail.com>
2025-02-28 02:27:57 +0700

    Remove test

--- api/Shipping/src/test/java/com/lemoo/shipping/ShippingApplicationTests.java
+++ api/Shipping/src/test/java/com/lemoo/shipping/ShippingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ShippingApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    //@Test
+   // void contextLoads() {
+    //}
 
 }

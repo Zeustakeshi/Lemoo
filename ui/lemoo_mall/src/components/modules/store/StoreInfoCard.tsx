43c10201b21c72055943506c2194bef5db691222
Minh Hiếu <gaconmvp2312@gmail.com>
2025-03-02 02:32:40 +0700

    Adds store follow/unfollow functionality
    Implements endpoints and UI for users to follow and unfollow stores.
    Includes error handling and feedback for the user.

--- ui/lemoo_mall/src/components/modules/store/StoreInfoCard.tsx
+++ ui/lemoo_mall/src/components/modules/store/StoreInfoCard.tsx
@@ -1,9 +1,69 @@
+import {
+    followStore,
+    getStoreFollowStatus,
+    unFollowStore,
+} from "@/api/store.api";
 import { StoreOverviewResponse } from "@/common/type/store.type";
 import { Button } from "@/components/ui/button";
-
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 type Props = { store: StoreOverviewResponse };
 
 const StoreInfoCard = ({ store }: Props) => {
+    const [isFollowed, setFollowed] = useState<boolean>(false);
+
+    const {
+        data: followStatusData,
+        refetch: refetchFollowStatus,
+        isLoading: fetchFollowStatusLoading,
+    } = useQuery({
+        queryKey: ["store-follow-status", store.id],
+        queryFn: async () => await getStoreFollowStatus(store.id),
+    });
+
+    const { mutateAsync: followStoreMutate, isPending: followStoreLoading } =
+        useMutation({
+            mutationKey: ["follow-store", store.id],
+            mutationFn: async () => await followStore(store.id),
+            onSuccess: () => setFollowed(true),
+        });
+
+    const {
+        mutateAsync: unfollowStoreMutate,
+        isPending: unfollowStoreLoading,
+    } = useMutation({
+        mutationKey: ["unfollow-store", store.id],
+        mutationFn: async () => await unFollowStore(store.id),
+        onSuccess: () => setFollowed(false),
+    });
+
+    useEffect(() => {
+        setFollowed(!!followStatusData);
+    }, [followStatusData]);
+
+    const handleFollowStore = async () => {
+        toast.promise(followStoreMutate(), {
+            loading: "Đang theo dõi...",
+            success: `Theo dõi cửa hàng thành công`,
+            error: `Theo dõi cửa hàng thất bại`,
+        });
+    };
+
+    const handleUnfollowStore = async () => {
+        toast.promise(unfollowStoreMutate(), {
+            loading: "Đang hủy theo dõi...",
+            success: `Hủy theo dõi cửa hàng thành công`,
+            error: `Hủy theo dõi cửa hàng thất bại`,
+        });
+    };
+
+    const handleToggleStoreFollow = async () => {
+        if (isFollowed) await handleUnfollowStore();
+        else await handleFollowStore();
+        await refetchFollowStatus();
+    };
+
     return (
         <div className="h-full  y relative !z-1 bg-white dark:bg-slate-800 shadow-md px-3 py-2 flex justify-between  w-[400px]">
             <div className="flex justify-start items-start flex-1  gap-2 ">
@@ -22,7 +82,17 @@ const StoreInfoCard = ({ store }: Props) => {
                 </div>
             </div>
             <div>
-                <Button>Theo dõi</Button>
+                <Button
+                    disabled={
+                        fetchFollowStatusLoading ||
+                        unfollowStoreLoading ||
+                        followStoreLoading
+                    }
+                    onClick={handleToggleStoreFollow}
+                    variant={isFollowed ? "secondary" : "default"}
+                >
+                    {isFollowed ? "Hủy theo dõi" : "Theo dõi"}
+                </Button>
             </div>
         </div>
     );
--- ui/lemoo_mall/src/main.tsx
+++ ui/lemoo_mall/src/main.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "./context/ThemeContext";
@@ -20,6 +21,7 @@ createRoot(document.getElementById("root")!).render(
                         <App></App>
                     </AuthProvider>
                 </Provider>
+                <Toaster position="bottom-right" reverseOrder={false} />
             </ThemeProvider>
             <ReactQueryDevtools
                 buttonPosition="bottom-left"

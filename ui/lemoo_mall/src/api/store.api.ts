65bb64e996d6cbd01f88da661f0ad8f975d8640b
Minh Hiếu <gaconmvp2312@gmail.com>
2025-03-02 02:32:40 +0700

    Adds store follow/unfollow functionality
    Implements endpoints and UI for users to follow and unfollow stores.
    Includes error handling and feedback for the user.

--- ui/lemoo_mall/src/api/store.api.ts
+++ ui/lemoo_mall/src/api/store.api.ts
@@ -6,3 +6,17 @@ export const getStoreOverviewById = async (
 ): Promise<StoreOverviewResponse> => {
     return await api.get(`/store/open/${storeId}`);
 };
+
+export const getStoreFollowStatus = async (
+    storeId: string
+): Promise<boolean> => {
+    return await api.get(`/store/${storeId}/follow/status`);
+};
+
+export const followStore = async (storeId: string) => {
+    return await api.post(`/store/${storeId}/follow`);
+};
+
+export const unFollowStore = async (storeId: string) => {
+    return await api.post(`/store/${storeId}/unfollow`);
+};

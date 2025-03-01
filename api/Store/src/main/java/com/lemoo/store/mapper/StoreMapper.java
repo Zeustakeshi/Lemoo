10eef3fd78b7b935aa7d88717f5bb82ccdfa04ca
Minh Hiếu <gaconmvp2312@gmail.com>
2025-03-01 22:01:45 +0700

    Adds public store API endpoints
    Implements a public API endpoint to retrieve store information.
    Creates necessary DTOs and service layer for public store access.
    Updates security configuration to allow access to public endpoints.

--- api/Store/src/main/java/com/lemoo/store/mapper/StoreMapper.java
+++ api/Store/src/main/java/com/lemoo/store/mapper/StoreMapper.java
@@ -7,13 +7,24 @@
 package com.lemoo.store.mapper;
 
 import com.lemoo.store.dto.response.InternalStoreResponse;
+import com.lemoo.store.dto.response.StorePublicResponse;
 import com.lemoo.store.dto.response.StoreResponse;
 import com.lemoo.store.entity.Store;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface StoreMapper {
+
     StoreResponse storeToStoreResponse(Store store);
+
+    default StorePublicResponse toStorePublicResponse(Store store) {
+        return StorePublicResponse.builder()
+                .id(store.getId())
+                .logo(store.getLogo())
+                .name(store.getName())
+                .follower((long) (store.getFollowers().size()))
+                .build();
+    }
 
     InternalStoreResponse toInternalStoreResponse(Store store);
 }

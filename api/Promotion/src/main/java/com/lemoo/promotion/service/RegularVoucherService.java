8696783318f7f6246d27407b0121a1f3b3aebfbc
Minh Hiếu <gaconmvp2312@gmail.com>
2025-03-04 13:30:24 +0700

    Adds endpoint to retrieve all regular vouchers
    Implements an endpoint to retrieve all regular vouchers associated with a specific store, with pagination support.
    
    This allows for easier management and display of vouchers within the store's administrative interface.

--- api/Promotion/src/main/java/com/lemoo/promotion/service/RegularVoucherService.java
+++ api/Promotion/src/main/java/com/lemoo/promotion/service/RegularVoucherService.java
@@ -8,12 +8,15 @@ package com.lemoo.promotion.service;
 
 import com.lemoo.promotion.dto.common.AuthenticatedAccount;
 import com.lemoo.promotion.dto.request.RegularVoucherRequest;
+import com.lemoo.promotion.dto.response.PageableResponse;
 import com.lemoo.promotion.dto.response.RegularVoucherResponse;
 
 public interface RegularVoucherService {
     String createVoucher(RegularVoucherRequest request, String storeId, AuthenticatedAccount account);
 
     RegularVoucherResponse findVoucherById(String voucherId, String storeId, AuthenticatedAccount account);
+
+    PageableResponse<RegularVoucherResponse> findAllByStoreId(String storeId, int page, int limit, AuthenticatedAccount account);
 
     void activateVoucher(String storeId, AuthenticatedAccount account, String voucherId);
 
--- api/Promotion/src/main/java/com/lemoo/promotion/service/impl/RegularVoucherServiceImpl.java
+++ api/Promotion/src/main/java/com/lemoo/promotion/service/impl/RegularVoucherServiceImpl.java
@@ -11,32 +11,60 @@ import com.lemoo.promotion.common.enums.VoucherType;
 import com.lemoo.promotion.dto.common.AuthenticatedAccount;
 import com.lemoo.promotion.dto.request.BaseVoucherRequest;
 import com.lemoo.promotion.dto.request.RegularVoucherRequest;
+import com.lemoo.promotion.dto.response.PageableResponse;
 import com.lemoo.promotion.dto.response.RegularVoucherResponse;
 import com.lemoo.promotion.entity.BaseVoucher;
 import com.lemoo.promotion.entity.RegularVoucher;
+import com.lemoo.promotion.mapper.PageMapper;
 import com.lemoo.promotion.mapper.VoucherMapper;
 import com.lemoo.promotion.repository.BaseVoucherRepository;
 import com.lemoo.promotion.service.RegularVoucherService;
 import com.lemoo.promotion.service.StoreService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegularVoucherServiceImpl extends BaseVoucherService implements RegularVoucherService {
 
     private final VoucherMapper voucherMapper;
+    private final BaseVoucherRepository baseVoucherRepository;
+    private final StoreService storeService;
+    private final PageMapper pageMapper;
 
     public RegularVoucherServiceImpl(
             BaseVoucherRepository voucherRepository,
             StoreService storeService,
-            VoucherMapper voucherMapper
+            VoucherMapper voucherMapper,
+            BaseVoucherRepository baseVoucherRepository,
+            PageMapper pageMapper
     ) {
         super(voucherRepository, storeService);
         this.voucherMapper = voucherMapper;
+        this.baseVoucherRepository = baseVoucherRepository;
+        this.storeService = storeService;
+        this.pageMapper = pageMapper;
     }
 
     @Override
     public String createVoucher(RegularVoucherRequest request, String storeId, AuthenticatedAccount account) {
         return super.createVoucher(storeId, account, request);
+    }
+
+    @Override
+    public PageableResponse<RegularVoucherResponse> findAllByStoreId(String storeId, int page, int limit, AuthenticatedAccount account) {
+        storeService.verifyStore(account.getId(), storeId);
+
+        PageRequest pageRequest = PageRequest.of(page, limit, Sort.by(Sort.Direction.DESC, "updatedAt"));
+
+        Page<BaseVoucher> baseVouchers = baseVoucherRepository
+                .findAllByStoreIdAndVoucherType(storeId, VoucherType.REGULAR_VOUCHER, pageRequest);
+
+        Page<RegularVoucherResponse> regularVouchers = baseVouchers
+                .map(baseVoucher -> voucherMapper.toRegularVoucherResponse((RegularVoucher) baseVoucher));
+
+        return pageMapper.toPageableResponse(regularVouchers);
     }
 
     @Override

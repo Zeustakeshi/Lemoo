1d3e2324224c9337a25c2957b5d4f85285a18ac4
Minh Hiếu <gaconmvp2312@gmail.com>
2025-03-04 15:42:28 +0700

    Initializes seller center UI with Shadcn
    Sets up the seller center UI using Shadcn, Tailwind CSS, and Radix UI.
    
    This includes adding necessary dependencies, configuring Tailwind CSS,
    defining aliases, and creating initial UI components such as buttons,
    forms, inputs, and labels. This structure will serve as the foundation
    for building the seller center's user interface.
    
    Adds a new route for creating regular vouchers.

--- ui/seller_center2/vite.config.ts
+++ ui/seller_center2/vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+    plugins: [react(), TanStackRouterVite(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });

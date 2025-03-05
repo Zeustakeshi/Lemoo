75e4fc8e444dd7427c2a541fe1799075d8f9643f
Minh Hiếu <gaconmvp2312@gmail.com>
2025-03-05 09:29:10 +0700

    Adds Voucher creation api
    Adds regular voucher creation feature, allowing users to create vouchers with defined discount types, limits and availabilities.

--- ui/seller_center2/src/routes/product/_product.addProduct.tsx
+++ ui/seller_center2/src/routes/product/_product.addProduct.tsx
@@ -2,13 +2,13 @@ import { createFileRoute } from "@tanstack/react-router";
 import FormAddProduct from "../../modules/product/FormAddProduct";
 
 export const Route = createFileRoute("/product/_product/addProduct")({
-  component: RouteComponent,
+    component: RouteComponent,
 });
 
 function RouteComponent() {
-  return (
-    <div>
-      <FormAddProduct />
-    </div>
-  );
+    return (
+        <div>
+            <FormAddProduct />
+        </div>
+    );
 }

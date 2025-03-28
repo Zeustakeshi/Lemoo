e91308175b8c52994861b8925c54027df7075c4a
Minh Hiếu <gaconmvp2312@gmail.com>
2025-03-28 23:02:24 +0700

    Fix shipping address endpoint

--- api/Shipping/src/main/java/com/lemoo/shipping/controller/BuyerShippingAddressController.java
+++ api/Shipping/src/main/java/com/lemoo/shipping/controller/BuyerShippingAddressController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController("/address")
 @RequiredArgsConstructor
 public class BuyerShippingAddressController {
     private final ShippingAddressService shippingAddressService;

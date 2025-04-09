3deb914879a8f8571003a4f168faf397c134a1d3
Vohoangtuanhdhtdm <121412803+Vohoangtuanhdhtdm@users.noreply.github.com>
2025-04-09 15:15:47 +0700

    refactor-status-order (#337)
    Co-authored-by: Vohoangtuanhdhtdm <2224801030161@student.tdmu.edu.vn>

--- ui/seller_center2/src/api/order.api.ts
+++ ui/seller_center2/src/api/order.api.ts
@@ -1,8 +1,22 @@
 import { OrderResponse } from "@/common/type/order.type";
 import { api } from "@/lib/api";
 
+export type OrderAction = "confirm" | "cancel" | "packed";
+
 export const getAllOrder = async (storeId: string): Promise<OrderResponse> => {
   return await api.get("/orders/seller", {
+    headers: {
+      "x-store-Id": storeId,
+    },
+  });
+};
+
+export const handleOrderAction = async (
+  orderId: string,
+  storeId: string,
+  action: OrderAction
+): Promise<OrderResponse> => {
+  return await api.get(`/orders/seller/${orderId}/${action}`, {
     headers: {
       "x-store-Id": storeId,
     },

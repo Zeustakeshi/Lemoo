8c0978899862a6d6a56a1b8d89d3f51bb6fd3821
Minh Hiếu <gaconmvp2312@gmail.com>
2025-04-14 01:41:50 +0700

    Adds shipping order creation endpoint
    Introduces the ability to create shipping orders via a new endpoint.
    
    This commit integrates with an external shipping provider to
    facilitate order creation. It sets up the necessary data structures
    and calls the external service to initiate the shipping process.

--- api/Shipping/src/main/java/com/lemoo/shipping/client/GhnClient.java
+++ api/Shipping/src/main/java/com/lemoo/shipping/client/GhnClient.java
@@ -8,9 +8,12 @@
 package com.lemoo.shipping.client;
 
 import com.lemoo.shipping.config.GhnRequestInterceptor;
+import com.lemoo.shipping.dto.request.NewShippingOrderRequest;
 import com.lemoo.shipping.dto.response.GhnApiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
@@ -22,6 +25,10 @@ import java.util.List;
         configuration = GhnRequestInterceptor.class
 )
 public interface GhnClient {
+
+    @PostMapping("/v2/shipping-order/create")
+    void createShippingOrder(@RequestBody NewShippingOrderRequest request);
+
     @GetMapping("/master-data/province")
     GhnApiResponse<List<HashMap<Object, Object>>> getProvince();
 

92ba5588d8f538247b99e955f8418922d68f259b
Minh Hiếu <gaconmvp2312@gmail.com>
2025-04-14 01:22:21 +0700

    Configures shipping service and dependencies
    Configures the shipping service to consume order creation events
    and create shipping orders.
    
    Updates the MongoDB connection URL and adds missing dependencies
    for mapstruct and lombok.
    Adds file encoding configuration.

--- api/Shipping/src/main/java/com/lemoo/shipping/event/consumer/OrderConsumer.java
+++ api/Shipping/src/main/java/com/lemoo/shipping/event/consumer/OrderConsumer.java
@@ -8,6 +8,7 @@
 package com.lemoo.shipping.event.consumer;
 
 import com.lemoo.shipping.event.model.NewShippingOrderEvent;
+import com.lemoo.shipping.service.ShippingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -16,8 +17,16 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class OrderConsumer {
 
+    private final ShippingService shippingService;
+
     @KafkaListener(topics = "shipping-service.shipping.create.failed", groupId = "${spring.kafka.consumer.group-id}")
     public void createShippingOrder(NewShippingOrderEvent event) {
-        
+        shippingService.createShippingOrder(
+                event.getOrderId(),
+                event.getStoreId(),
+                event.getUserId(),
+                event.getShippingAddressId(),
+                event.getSkus()
+        );
     }
 }

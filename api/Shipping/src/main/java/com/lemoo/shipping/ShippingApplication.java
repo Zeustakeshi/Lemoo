bbfbea9e13c95a575540c84231908e920aa293c7
Minh Hiếu <gaconmvp2312@gmail.com>
2025-04-14 10:06:46 +0700

    Update redis config shipping service

--- api/Shipping/src/main/java/com/lemoo/shipping/ShippingApplication.java
+++ api/Shipping/src/main/java/com/lemoo/shipping/ShippingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication()
 @EnableAsync
 @EnableFeignClients
 public class ShippingApplication {

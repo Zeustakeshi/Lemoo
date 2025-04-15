49e41068d94656deee01227c7540b90752c767d3
Minh Hiếu <gaconmvp2312@gmail.com>
2025-04-16 00:27:19 +0700

    Removes cart items after successful order
    Implements functionality to remove cart items associated with an order after the product reservation process is completed successfully.
    
    Adds an endpoint to remove a specific cart item.

--- api/Order_v2/src/main/java/com/lemoo/order_v2/service/CartItemService.java
+++ api/Order_v2/src/main/java/com/lemoo/order_v2/service/CartItemService.java
@@ -11,8 +11,14 @@ import com.lemoo.order_v2.dto.request.AddToCartRequest;
 import com.lemoo.order_v2.dto.response.CartItemResponse;
 import com.lemoo.order_v2.dto.response.PageableResponse;
 
+import java.util.Set;
+
 public interface CartItemService {
     String addToCart(AddToCartRequest request, AuthenticatedAccount account);
 
     PageableResponse<CartItemResponse> getUserCart(int page, int limit, AuthenticatedAccount account);
+
+    void removeCartItemBySkuCodes(String userId, Set<String> skuCodes);
+
+    void removeCartItemById(String cartItemId, AuthenticatedAccount account);
 }

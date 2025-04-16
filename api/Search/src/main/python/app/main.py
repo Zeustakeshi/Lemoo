c643ccf1fb8151acfa62137ae2f26316e5ca4f61
Minh Hieu <95726917+Zeustakeshi@users.noreply.github.com>
2025-04-17 01:42:55 +0700

    Create search service (#427)
    * track-the-order (#421)
    
    Co-authored-by: Vohoangtuanhdhtdm <2224801030161@student.tdmu.edu.vn>
    
    * Fix product_analysis_consumer.py file encoding issue
    
    * Adds Search API module
    
    Introduces a new Search API module to enable product search functionality.
    
    The module includes:
    - API endpoints for handling search requests.
    - Configurations for embedding and vector database.
    - Services for analyzing user queries and performing vector store searches.
    - Models for request and response objects.
    
    The search functionality leverages Gemini and Qdrant to provide relevant search results.
    
    * Adds search service deployment workflow
    
    Sets up a CI workflow to build and push a Docker image for the search service to Docker Hub.
    
    This automated process streamlines the deployment of search service updates by building a Docker image upon pull requests to the main branch that modify the search service code.
    
    ---------
    
    Co-authored-by: Vohoangtuanhdhtdm <121412803+Vohoangtuanhdhtdm@users.noreply.github.com>
    Co-authored-by: Vohoangtuanhdhtdm <2224801030161@student.tdmu.edu.vn>

---
+++ api/Search/src/main/python/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from src.main.python.app.api.v1 import search_router
+
+app = FastAPI()
+
+app.include_router(search_router.router)
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)

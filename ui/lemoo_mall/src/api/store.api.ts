d12e3a7988722cfe9778fb20aee0afa3c92b9c7b
Vohoangtuanhdhtdm <121412803+Vohoangtuanhdhtdm@users.noreply.github.com>
2025-04-17 16:22:10 +0700

    refactor-detail-product3 (#446)
    Co-authored-by: Vohoangtuanhdhtdm <2224801030161@student.tdmu.edu.vn>

--- ui/lemoo_mall/src/api/store.api.ts
+++ ui/lemoo_mall/src/api/store.api.ts
@@ -2,21 +2,21 @@ import { StoreOverviewResponse } from "@/common/type/store.type";
 import { api } from "@/lib/api";
 
 export const getStoreOverviewById = async (
-    storeId: string
+  storeId: string
 ): Promise<StoreOverviewResponse> => {
-    return await api.get(`/store/open/${storeId}`);
+  return await api.get(`/store/open/${storeId}`);
 };
 
 export const getStoreFollowStatus = async (
-    storeId: string
+  storeId: string
 ): Promise<boolean> => {
-    return await api.get(`/store/${storeId}/follow/status`);
+  return await api.get(`/store/${storeId}/follow/status`);
 };
 
 export const followStore = async (storeId: string) => {
-    return await api.post(`/store/${storeId}/follow`);
+  return await api.post(`/store/${storeId}/follow`);
 };
 
 export const unFollowStore = async (storeId: string) => {
-    return await api.post(`/store/${storeId}/unfollow`);
+  return await api.post(`/store/${storeId}/unfollow`);
 };

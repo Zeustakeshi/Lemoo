4cef0f15745dbdcdcfc53b49f593c672844604f3
Minh Hiếu <gaconmvp2312@gmail.com>
2025-04-17 10:17:01 +0700

    Fix seller center refesh token

--- ui/seller_center2/src/lib/refreshToken.ts
+++ ui/seller_center2/src/lib/refreshToken.ts
@@ -1,42 +1,46 @@
 import memoize from "memoizee";
 import * as tokenStore from "./tokenStore";
 
-import { clearLocalStorage, clearSessionStorage } from "./storage";
+import { redirectToSSO } from "@/helpers/utils/redirectToSSO";
 import { TokenType } from "../common/enum/token.enum";
 import { api } from "./api";
+import { clearLocalStorage, clearSessionStorage } from "./storage";
 
 const clearAuth = () => {
-  clearLocalStorage();
-  clearSessionStorage();
-  tokenStore.removeToken(TokenType.ACCESS_TOKEN);
-  tokenStore.removeToken(TokenType.REFRESH_TOKEN);
+    clearLocalStorage();
+    clearSessionStorage();
+    tokenStore.removeToken(TokenType.ACCESS_TOKEN);
+    tokenStore.removeToken(TokenType.REFRESH_TOKEN);
 };
 
 const refreshToken = async () => {
-  const refreshToken = await tokenStore.getTokenValue(TokenType.REFRESH_TOKEN);
-  if (!refreshToken) {
-    clearAuth();
-    return;
-  }
-  alert("Refresh token");
-  try {
-    const data: any = await api({
-      method: "POST",
-      url: "/token/refresh",
-      data: {
-        refreshToken: refreshToken,
-      },
-    });
+    const refreshToken = await tokenStore.getTokenValue(
+        TokenType.REFRESH_TOKEN
+    );
+    if (!refreshToken) {
+        clearAuth();
+        return;
+    }
+    alert("Refresh token");
+    try {
+        const data: any = await api({
+            method: "POST",
+            url: "/token/refresh",
+            data: {
+                refreshToken: refreshToken,
+            },
+        });
 
-    await tokenStore.saveToken(data.accessToken);
-    await tokenStore.saveToken(data.refreshToken);
-  } catch (error) {
-    console.log("Refresh token error " + error);
-    clearAuth();
-  }
+        await tokenStore.saveToken(data.accessToken);
+        await tokenStore.saveToken(data.refreshToken);
+    } catch (error) {
+        console.log("Refresh token error " + error);
+        clearAuth();
+        redirectToSSO();
+    }
 };
 
 const maxAge = 10000;
 export const memoizedRefreshToken = memoize(refreshToken, {
-  maxAge,
+    maxAge,
 });

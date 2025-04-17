b193f2235290c46c0f6f816a93d1b40c2ca3411a
Minh Hiếu <gaconmvp2312@gmail.com>
2025-04-17 13:01:12 +0700

    Updates order status based on shipping events
    Adds a Kafka consumer to listen for shipping status updates from the shipping service.
    
    The order service now updates the order status based on the shipping status events it receives, such as "in transit", "delivered", or "failed delivery".

--- api/Order_v2/src/main/java/com/lemoo/order_v2/event/consumer/ShippingConsumer.java
+++ api/Order_v2/src/main/java/com/lemoo/order_v2/event/consumer/ShippingConsumer.java
@@ -9,6 +9,7 @@ package com.lemoo.order_v2.event.consumer;
 
 import com.lemoo.order_v2.common.enums.OrderStatus;
 import com.lemoo.order_v2.event.model.CreateShippingOrderResultEvent;
+import com.lemoo.order_v2.event.model.UpdateOrderShippingStatusEvent;
 import com.lemoo.order_v2.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -28,5 +29,17 @@ public class ShippingConsumer {
     @KafkaListener(topics = "shipping-service.shipping.create.failed", groupId = "${spring.kafka.consumer.group-id}")
     public void createShippingOrderFailed(CreateShippingOrderResultEvent event) {
         orderService.updateOrderStatus(event.getUserId(), event.getOrderId(), OrderStatus.FAILED_DELIVERY);
+    }
+
+    @KafkaListener(topics = "shipping-service.shipping.status.update", groupId = "${spring.kafka.consumer.group-id}")
+    public void updateOrderShippingStatus(UpdateOrderShippingStatusEvent event) {
+        if (event.getShippingStatus().equals(UpdateOrderShippingStatusEvent.ShippingStatus.IN_TRANSIT)) {
+            orderService.updateOrderStatus(event.getUserId(), event.getOrderId(), OrderStatus.IN_TRANSIT);
+        } else if (event.getShippingStatus().equals(UpdateOrderShippingStatusEvent.ShippingStatus.DELIVERED)) {
+            orderService.updateOrderStatus(event.getUserId(), event.getOrderId(), OrderStatus.DELIVERED);
+            // TODO: notify to user review product
+        } else {
+            orderService.updateOrderStatus(event.getUserId(), event.getOrderId(), OrderStatus.FAILED_DELIVERY);
+        }
     }
 }

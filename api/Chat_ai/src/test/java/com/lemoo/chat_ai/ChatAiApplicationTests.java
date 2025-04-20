682b8a005acb646e5e8d90fe0a590801f77531af
Minh Hiếu <gaconmvp2312@gmail.com>
2025-04-20 10:03:53 +0700

    Remove test

--- api/Chat_ai/src/test/java/com/lemoo/chat_ai/ChatAiApplicationTests.java
+++ api/Chat_ai/src/test/java/com/lemoo/chat_ai/ChatAiApplicationTests.java
@@ -1,13 +1,12 @@
 package com.lemoo.chat_ai;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ChatAiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }

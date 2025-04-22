4fb02b685b0cfbcee23f5a1794b1d5512ed302b5
Minh Hiếu <gaconmvp2312@gmail.com>
2025-04-22 16:08:00 +0700

    Enhances Chat AI with user context and tools
    Improves the Chat AI's ability to personalize responses by providing user information as context, ensuring sensitive data is not exposed in the response.
    
    Additionally, this change adds order service to the available tools.

--- api/Chat_ai/src/main/java/com/lemoo/chat_ai/config/ChatClientConfig.java
+++ api/Chat_ai/src/main/java/com/lemoo/chat_ai/config/ChatClientConfig.java
@@ -20,13 +20,17 @@ public class ChatClientConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder, List<McpSyncClient> mcpSyncClients) {
+        for (var client : mcpSyncClients) {
+            System.out.println("client info:" + client.getClientInfo());
+            System.out.println("tools" + client.listTools());
+        }
         return chatClientBuilder
                 .defaultTools(new SyncMcpToolCallbackProvider(mcpSyncClients))
                 .defaultSystem("""
-                        You are a virtual shopping assistant for users on the Lemoo e-commerce platform. 
-                        Your role is to enhance the user's shopping experience by leveraging available tools to interact with the system. 
-                        Your responsibilities include: searching for orders, creating shopping plans, reviewing user orders, and managing promotions for users. 
-                        If a user asks questions unrelated to these tasks, politely decline to assist, keeping your focus on shopping-related functionalities.           
+                        You are a virtual shopping assistant for users on the Lemoo e-commerce platform.
+                        Your role is to enhance the user's shopping experience by leveraging available tools to interact with the system.
+                        Your responsibilities include: searching for orders, creating shopping plans, reviewing user orders, and managing promotions for users.
+                        If a user asks questions unrelated to these tasks, politely decline to assist, keeping your focus on shopping-related functionalities.
                         """)
                 .build();
     }
